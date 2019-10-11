be79a0350b4f370b98691bcefbfabd92357db803
Pavan Kunchala <pavankuchalapk@gmail.com>
2019-10-11 23:48:50 +0530

    Gane pad
    added both WASD abd mouse movemenr

--- RPG/Assets/Scripts/Camera/CursorAffordance.cs
+++ RPG/Assets/Scripts/Camera/CursorAffordance.cs
@@ -31,7 +31,7 @@ public class CursorAffordance : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         switch(cameraRaycaster.layerHit)
         {
--- RPG/Assets/Scripts/Player/PlayerMovement.cs
+++ RPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
 
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
@@ -8,9 +9,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float walkMoveStopRadius = .2f;
 
+    // to check wheter he is in direct mode or not
+    private bool isIndirectMode = false;
+
 
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
-    CameraRaycaster cameraRaycaster;
+    CameraRaycaster cameraRaycaster; 
     Vector3 currentClickTarget;
         
     private void Start()
@@ -23,10 +27,43 @@ public class PlayerMovement : MonoBehaviour
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
+
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            isIndirectMode = !isIndirectMode; // toggle mode
+        }
+
+        if(isIndirectMode)
+        {
+            DirectMovement();
+        }
+        else 
+        {
+            MouseMovement();// Mouse moment
+        }
+       
+
+    }
+
+    private void DirectMovement()
+    {
+
+        print("DirectMovement");
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+      Vector3  m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+       Vector3  m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void MouseMovement()
+    {
         if (Input.GetMouseButton(0))
         {
             print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
-            switch(cameraRaycaster.layerHit)
+            switch (cameraRaycaster.layerHit)
             {
                 case Layer.Walkable:
                     currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case                 
@@ -40,11 +77,11 @@ public class PlayerMovement : MonoBehaviour
                     return;
             }
 
-            
+
         }
         var playerToClickPoint = currentClickTarget - transform.position;
 
-        if(playerToClickPoint.magnitude >= walkMoveStopRadius)
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
         {
             m_Character.Move(playerToClickPoint, false, false);
         }
@@ -52,9 +89,6 @@ public class PlayerMovement : MonoBehaviour
         {
             m_Character.Move(Vector3.zero, false, false);
         }
-
-
-       
     }
 }
 
